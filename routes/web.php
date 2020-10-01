619c7215c03e8317f59ce6085b032b208f92bfe1
Riazul Hasan <riazul701@gmail.com>
2020-10-01 12:10:16 +0600

    Laravel 7/6 Import Export Excel & CSV File Tutorial.
    Tutorial Web Address:
    https://www.itsolutionstuff.com/post/laravel-6-import-export-excel-csv-file-tutorialexample.html

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('importExportView', 'MyController@importExportView');
+Route::get('export', 'MyController@export')->name('export');
+Route::post('import', 'MyController@import')->name('import');
