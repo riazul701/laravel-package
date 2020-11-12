589cc9e9fc62ad01331879c8936756af25e4e108
Riazul Hasan <riazul701@gmail.com>
2020-11-12 20:21:25 +0600

    Image Upload and Resize Example using Intervention Image Package.
    Tutorial Web Address:
    https://www.itsolutionstuff.com/post/laravel-5-image-upload-and-resize-example-using-intervention-image-packageexample.html

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('resizeImage', 'ImageController@resizeImage');
+
+Route::post('resizeImagePost',['as'=>'resizeImagePost','uses'=>'ImageController@resizeImagePost']);
