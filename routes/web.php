9ac854bf4e6ce6e63d9697c389c4b0efcbb20ab9
Riazul Hasan <riazul701@gmail.com>
2020-12-23 00:44:49 +0600

    Ajax crop image before upload using croppie plugin.
    Tutorial Web Address:
    https://www.itsolutionstuff.com/post/laravel-5-ajax-crop-image-before-upload-using-using-croppie-pluginexample.html

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('image-crop', 'ImageController@imageCrop');
+Route::post('image-crop', 'ImageController@imageCropPost');
