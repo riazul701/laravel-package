7eab3dbd1b2e26f0fafbb64eefdc258746f44184
Riazul Hasan <riazul701@gmail.com>
2021-01-12 08:59:18 +0600

    Create url shortener using Laravel.
    Tutorial Web Address:
    https://www.itsolutionstuff.com/post/how-to-create-url-shortener-using-laravelexample.html

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('generate-shorten-link', 'ShortLinkController@index');
+Route::post('generate-shorten-link', 'ShortLinkController@store')->name('generate.shorten.link.post');
+
+Route::get('{code}', 'ShortLinkController@shortenLink')->name('shorten.link');
