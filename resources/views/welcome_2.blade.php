216368f06ca9096dcc6b10c91d66bfca06b9c332
Riazul Hasan <riazul701@gmail.com>
2021-01-15 20:27:06 +0600

    How to Install and Use Moment JS in Laravel?
    Tutorial Web Address:
    https://www.itsolutionstuff.com/post/how-to-install-and-use-moment-js-in-laravelexample.html

---
+++ resources/views/welcome_2.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link type="text/css" rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
+</head>
+<body>
+
+<h1>How to use moment js in Laravel? - ItSolutionStuff.com</h1>
+
+</body>
+</html>
