8ba95475480ec04ba4bc993d62f26b1093677d3f
Riazul Hasan <riazul701@gmail.com>
2021-01-16 22:39:02 +0600

    How to Use Bootstrap WYSIHTML5 Editor in Laravel?
    Tutorial Web Address:
    https://www.itsolutionstuff.com/post/how-to-use-bootstrap-wysihtml5-editor-in-laravelexample.html

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/', function (Request $request) {
+    return $request->description;
 });
